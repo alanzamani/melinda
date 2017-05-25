4e38759ffd433fc33f79e5babe7ef2ddd1e1eb30
Ali Zamani <alizamani@Alis-MacBook-Pro.local>
2017-05-25 21:48:28 +1000

    primary dynamic application

--- script/script.js
+++ script/script.js
@@ -1,8 +1,41 @@
-function doit() {
+$("document").ready(function() {
 
-}
+    function addMsg(user, name, msg, timer) {
+        var elBox = document.createElement("div");
+        var el1 = document.createElement("span");
+        var el2= document.createElement("p");
+        elBox.style.borderRadius = "3px";
+        elBox.style.padding = "7px";
+        setTimeout(function () {
+            if (user === "client"){
+                elBox.style.background = "#eee";
+            }
+            el1.innerHTML = name;
+            el2.innerHTML = msg;
+        },timer);
+        $("#content").append(elBox);
+        elBox.append(el1, el2);
+    }
 
 
-window.onload = function () {
-    doit();
-}
+    $("#chat-now").click(function () {
+        $("#chat-now").css("visibility", "hidden");
+        $("#cover-msg").remove(0);
+        //$("#cover-msg").html("");
+        $("#form").css("visibility", "visible");
+        addMsg("agent", "Mary: ", '"Hi Nicolas this is Mary from Abank. How may I help ypu today?"', 1000);
+    });
+
+
+    $("#form").submit(function () {
+        var hook = $("#in").val();
+        //alert(hook);
+        addMsg("client", 'Nicholas: "', hook+'"', 0);
+        addMsg("agent", 'Mary: "', 'To reset your password just click' +
+            'on the My details menu. Once you have clicked the menu a new page will' +
+            'appear Select the link Change password". om this link it will take' +
+            'you to a screen where you can change your password"', 3000);
+    });
+
+});
+
